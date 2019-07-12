4d0a031a04d343c801a9801c5cf41e1a1e798453
Michael Lekrans <mlekrans@hotmail.com>
2019-07-12 07:03:14 +0200

    Initial Commit

---
+++ IOS_Architecture_events/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS_Architecture_events
+//
+//  Created by Michael Lekrans on 2019-07-12.
+//  Copyright © 2019 Michael Lekrans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
