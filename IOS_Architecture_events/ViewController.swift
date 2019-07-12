825842d0bc1a7fe715ae19821d2d1a34b31b0e43
Michael Lekrans <mlekrans@hotmail.com>
2019-07-12 07:17:21 +0200

    First Commit

--- IOS_Architecture_events/ViewController.swift
+++ IOS_Architecture_events/ViewController.swift
@@ -13,6 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("ViewController.swift -> viewDidLoad")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("ViewController.swift -> viewDidAppear")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("ViewController.swift -> viewWillAppear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("ViewController.swift -> viewDidDisappear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("ViewController.swift -> viewWillDisappear")
     }
 
 
